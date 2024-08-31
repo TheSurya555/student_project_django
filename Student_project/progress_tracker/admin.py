4bf251058a491a229efd1fec83b79e4f8beaa48f
Surya <124416983+TheSurya555@users.noreply.github.com>
2024-08-31 18:02:15 +0530

    add the functionality of the admin notification

--- Student_project/progress_tracker/admin.py
+++ Student_project/progress_tracker/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Project, Progress
+from notifications.signals import notify
+from django.utils.translation import gettext_lazy as _
 
 class ProgressInline(admin.TabularInline):
     model = Progress
@@ -17,11 +19,34 @@ class ProjectAdmin(admin.ModelAdmin):
 
     display_progress_stages.short_description = 'Progress Stages'
 
+    def save_model(self, request, obj, form, change):
+        # Save the project instance
+        super().save_model(request, obj, form, change)
+
+        # Send notifications
+        recruiter = obj.client  # Assuming this is a User instance
+        candidate = obj.user  # Assuming this is a User instance
+
+        if recruiter:
+            notify.send(
+                sender=request.user,
+                recipient=recruiter,
+                verb=_("Your project '{0}' has been assigned to candidate: {1}").format(obj.project_name, candidate.username),
+                description="The candidate will be responsible for executing specific tasks within the project. Please collaborate closely to ensure all objectives are met.",
+                target=obj
+            )
+
+        if candidate:
+            notify.send(
+                sender=request.user,
+                recipient=candidate,
+                verb=_("You have been assigned a new project: {0}").format(obj.project_name),
+                description="Your role is critical in achieving the project's goals. Please review the project tasks and communicate with the recruiter to align on expectations and deadlines.",
+                target=obj
+            )
+
 @admin.register(Progress)
 class ProgressAdmin(admin.ModelAdmin):
     list_display = ('project', 'stage', 'is_completed', 'user')
     list_filter = ('is_completed', 'project__project_name')
     search_fields = ('stage',)
-
-
-# minor chages is needed    
